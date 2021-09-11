5ee3e86593ae27de1c522d13cfb61c0a098936de
Ricky <ricky.defazio@volusion.com>
2021-09-11 14:15:01 -0400

    modify production class names

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-// Triggering workflow
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
-        <BrowserRouter>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
             <Route exact path='/' component={Landing} />
           </Switch>
-        </BrowserRouter>
-      </StylesProvider>
+        </StylesProvider>
+      </BrowserRouter>
     </div>
   );
 };
