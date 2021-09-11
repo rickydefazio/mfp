39377d35c707f0eed68b29d82ce35c514de7aeb6
Ricky <ricky.defazio@volusion.com>
2021-09-11 17:18:31 -0400

    add multi-tier navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './app';
 
-export function mount(el) {
-  ReactDOM.render(<App />, el);
+export function mount(el, { onNavigate, defaultHistory }) {
+  const history = defaultHistory ?? createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 }
 
 if (process.env.NODE_ENV === 'development') {
   const marketingEl = document.querySelector('#marketing-dev');
 
   if (marketingEl) {
-    mount(marketingEl);
+    mount(marketingEl, { defaultHistory: createBrowserHistory });
   }
 }
